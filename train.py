46fe855123a5ebd3b33d31636ce3fd302955bf10
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-12 22:27:26 +0700

    test

--- train.py
+++ train.py
@@ -172,8 +172,8 @@ def main():
             # analysis gradient flow 
             loss_analysis = model(batch)['loss']
             params_encoder, params_decoder = list(model.encoder.parameters()), list(model.decoder.parameters())
-            g_encoder = torch.autograd.grad(loss_analysis, params_encoder, retain_graph=True)
-            g_decoder = torch.autograd.grad(loss_analysis, params_decoder, retain_graph=True)
+            g_encoder = torch.autograd.grad(loss_analysis, params_encoder, retain_graph=True, allow_unused=True)
+            g_decoder = torch.autograd.grad(loss_analysis, params_decoder, retain_graph=True, allow_unused=True)
             f_g_encoder = flat(g_encoder)
             f_g_decoder = flat(g_decoder)
             cos_sim = torch.dot(f_g_decoder, f_g_encoder) / (f_g_encoder.norm() * f_g_decoder.norm() + 1e-8)

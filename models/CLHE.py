3e5f02beb77e9fcbf9bd1d79794e81efb42960db
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-29 09:08:48 +0700

    test

--- models/CLHE.py
+++ models/CLHE.py
@@ -921,8 +921,8 @@ class CLHE(nn.Module):
         )
 
         # option 1 
-        bundle_feature = bundle_feature + bundle_gat_emb[idx] + bundle_modal_emb[idx]
-        feat_retrival_view = feat_retrival_view + item_gat_emb + item_modal_emb
+        bundle_feature = bundle_feature + bundle_gat_emb[idx] 
+        feat_retrival_view = feat_retrival_view + item_gat_emb 
         # bundle_feature = bundle_feature + bundle_f[idx]
         # feat_retrival_view = feat_retrival_view + item_f
         main_score = bundle_feature @ feat_retrival_view.transpose(0, 1)

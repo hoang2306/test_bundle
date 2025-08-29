c3ccacdb8ce0988f9cad330c713c78a5f0e081ef
Pham Duc Hoang <hoangggp@gmail.com>
2025-08-29 08:51:28 +0700

    test

--- models/CLHE.py
+++ models/CLHE.py
@@ -792,8 +792,8 @@ class CLHE(nn.Module):
         feat_retrival_view, item_gat_emb, item_modal_emb, cross_modal_item_emb, _, item_f = self.decoder(batch, all=True)
 
         # option 1 
-        bundle_feature = bundle_feature + bundle_gat_emb[idx] + bundle_modal_emb[idx]
-        feat_retrival_view = feat_retrival_view + item_gat_emb + item_modal_emb
+        bundle_feature = bundle_feature + bundle_gat_emb[idx] 
+        feat_retrival_view = feat_retrival_view + item_gat_emb 
         # bundle_feature = bundle_feature + bundle_f[idx]
         # feat_retrival_view = feat_retrival_view + item_f
         main_score = bundle_feature @ feat_retrival_view.transpose(0, 1) 
@@ -900,7 +900,7 @@ class CLHE(nn.Module):
 
 
         combine_loss = {
-            'loss': loss + item_loss,
+            'loss': loss,
             'item_loss': loss,
             'bundle_loss': loss
         }

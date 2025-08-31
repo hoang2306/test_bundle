c854e1591ec2df2cfc70f2502d0cf0cd43c6b0df
Pham Duc Hoang <hoangggp@gmail.com>
2025-09-01 00:34:22 +0700

    test

--- models/CLHE.py
+++ models/CLHE.py
@@ -923,17 +923,20 @@ class CLHE(nn.Module):
         #     cat_prob = (logits * mask).sum(dim=-1, keepdim=True)
         #     all_probs.append(cat_prob)
 
-        item2cate = torch.tensor(
-            [self.item_id_2_cate[item_id] for item_id in range(self.num_item)],
+        item_in_batch = torch.argwhere(full.sum(dim=0)).squeeze()
+        item2cate_in_batch = torch.tensor(
+            [self.item_id_2_cate[item_id.item()] for item_id in item_in_batch],
             device=self.device
         )
 
         # one-hot mask: [num_item, num_cate]
-        mask_matrix = torch.nn.functional.one_hot(item2cate, num_classes=len(self.item_id_2_cate)).float()
+        mask_matrix = torch.nn.functional.one_hot(
+            item2cate_in_batch, num_classes=len(self.item_id_2_cate)
+        ).float()
 
         # logits: [batch_size, num_item]
         # all_probs: [batch_size, num_cate]
-        all_probs = logits @ mask_matrix
+        all_probs = logits[:, item_in_batch] @ mask_matrix
 
         # all_probs: [n_bundle, n_cate]
         print(f'all_probs: {all_probs}')

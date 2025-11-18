dbe3bc6503364e8e069d90524b56935d723c3389
hoang2306 <hoangggp@gmail.com>
2025-11-19 02:41:39 +0700

    test'

--- models/CLHE.py
+++ models/CLHE.py
@@ -1027,8 +1027,8 @@ class CLHE(nn.Module):
 
 
         combine_loss = {
-            'loss': loss + item_loss + bundle_loss - 0.01*entropy_cate_,
-            # 'loss': loss,
+            # 'loss': loss + item_loss + bundle_loss - 0.01*entropy_cate_,
+            'loss': loss,
             'item_loss': loss,
             'bundle_loss': loss
         }

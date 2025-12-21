7d2688bb7f8ab32c81a5c73ecff02380ac31ab6d
hoang2306 <hoangggp@gmail.com>
2025-12-21 10:30:36 +0700

    test

--- parse.py
+++ parse.py
@@ -96,6 +96,7 @@ def get_cmd():
     parser.add_argument("--type_adapter", default="linear", choices=['MLP', 'linear', 'MoE'], type=str, help="type of adapter for bundle summary emb")
     parser.add_argument("--alpha_bundle_sum", default=0.2, type=float, help="")
     parser.add_argument("--alpha_balance_loss", default=0.1, type=float, help="")
+    parser.add_argument("--alpha_noise_moe", default=0.1, type=float, help="")
 
     args = parser.parse_args()
     return args

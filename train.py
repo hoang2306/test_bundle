3a48ed50549c4c119cf1f66a5396761eecb55841
hoang2306 <hoangggp@gmail.com>
2026-01-25 23:46:55 +0700

    test

--- train.py
+++ train.py
@@ -155,6 +155,15 @@ def get_cmd():
     parser.add_argument("--early_stop_max_epoch", default=20, type=int, help="num of early stopping epoch")
 
 
+    # for LLM bundle 
+    parser.add_argument('--bundle_sum_emb_path', type=str, default="", help='path to bundle summary embeddings')
+    # moe 
+    parser.add_argument("--type_adapter", default="linear", choices=['MLP', 'linear', 'MoE'], type=str, help="type of adapter for bundle summary emb")
+    parser.add_argument("--alpha_bundle_sum", default=0.2, type=float, help="")
+    parser.add_argument("--alpha_balance_loss", default=0.1, type=float, help="")
+    parser.add_argument("--alpha_noise_moe", default=0.1, type=float, help="")
+
+
     args = parser.parse_args()
     return args
 

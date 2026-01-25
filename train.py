7439855dab3113cbd5afe015924dab3914fbc5b7
hoang2306 <hoangggp@gmail.com>
2026-01-26 01:58:41 +0700

    test

--- train.py
+++ train.py
@@ -240,7 +240,7 @@ def main():
     run = SummaryWriter(run_path)
     try:
         model = getattr(models, conf['model'])(
-            conf, dataset.graphs, dataset.features, dataset.cate).to(device)
+            conf, dataset.graphs, dataset.features).to(device)
     except:
         raise ValueError("Unimplemented model %s" % (conf["model"]))
 

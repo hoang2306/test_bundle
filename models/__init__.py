265130b2963d80a863e4cb99d667c53cbe664a03
hoang2306 <hoangggp@gmail.com>
2026-01-28 08:40:29 +0700

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 # from .CLHE import CLHE
-from .CLHE_LLM import CLHE 
-# from .CLHE_shared_encoder import CLHE 
+# from .CLHE_LLM import CLHE 
+from .CLHE_shared_encoder import CLHE 
 
 from utility import slash, setup_seed
